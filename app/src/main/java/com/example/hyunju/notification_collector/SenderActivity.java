56d178f9a40eeef18301fb5eb468feb1101607a0
jioo123 <jungjioo@jeongjiuui-MacBook-Pro.local>
2019-04-20 01:51:46 +0900

    telegram

--- app/src/main/java/com/example/hyunju/notification_collector/SenderActivity.java
+++ app/src/main/java/com/example/hyunju/notification_collector/SenderActivity.java
@@ -15,6 +15,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.hyunju.notification_collector.telegram.TgUtils;
 import com.example.hyunju.notification_collector.utils.FileUtils;
 import com.example.hyunju.notification_collector.utils.SendFacebookMessage;
 import com.example.hyunju.notification_collector.utils.SendMail;
@@ -117,6 +118,7 @@ public class SenderActivity extends AppCompatActivity implements View.OnClickLis
 
                     recipientDialog.show();
                 } else if (pos == 2) { // telegram
+                    TgUtils.sendMessage(-1,"내용");
 
                 } else if(pos == 3) { // 이메일 부분
                     if(email != null) { // 사용자 이메일이 저장되어있는 경우
