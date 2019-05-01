61319d28dbb2497ce1121ca36f1b2a6a20b2dc61
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-05-01 19:49:52 +0900

    add mail detail activity

---
+++ app/src/main/java/com/example/hyunju/notification_collector/MailDetailActivity.java
@@ -0,0 +1,40 @@
+package com.example.hyunju.notification_collector;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MailDetailActivity extends AppCompatActivity {
+
+    private TextView textViewFrom;
+    private TextView textViewSubject;
+    private TextView textViewDate;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maildetail);
+
+        textViewFrom = (TextView) findViewById(R.id.textViewFrom);
+        textViewSubject = (TextView) findViewById(R.id.textViewSubject);
+        textViewDate = (TextView) findViewById(R.id.textViewDate);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        Intent intent = getIntent();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy년 MM월 dd일 HH:mm");
+
+        textViewFrom.setText(intent.getExtras().getString("from"));
+        textViewSubject.setText(intent.getExtras().getString("subject"));
+
+        textViewDate.setText(df.format((Date) intent.getSerializableExtra("date")));
+        webView.loadData(intent.getExtras().getString("body"), "text/html", "UTF-8");
+
+    }
+}
