317a2cca3c7da85a524a03f42e8f25bf175496e2
JuYeong <pyeonjy97@gmail.com>
2019-05-06 20:23:13 +0900

    send message to people & add reply flag

--- app/src/main/java/com/example/hyunju/notification_collector/SendToGroupActivity.java
+++ app/src/main/java/com/example/hyunju/notification_collector/SendToGroupActivity.java
@@ -1,9 +1,11 @@
 package com.example.hyunju.notification_collector;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hyunju.notification_collector.models.Contact;
 
@@ -23,9 +26,11 @@ public class SendToGroupActivity extends AppCompatActivity {
 
     private ListView lv_recipients;
     private EditText et_msg;
-    private Button btn_send;
+    private Button btn_attachment, btn_send;
 
+    private ListViewAdapter adapter;
     private ArrayList<Contact> contacts;
+    private ArrayList<Boolean> didSend;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,9 +38,30 @@ public class SendToGroupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_send_to_group);
 
         lv_recipients = findViewById(R.id.lv_recipients);
+        et_msg = findViewById(R.id.et_msg);
+        btn_attachment = findViewById(R.id.btn_attachment);
+        btn_send = findViewById(R.id.btn_send);
+        btn_send.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SmsManager smsManager = SmsManager.getDefault();
+                for (Contact contact : contacts) {
+                    smsManager.sendTextMessage(
+                            contact.getPhonenum(), null, et_msg.getText().toString(), null, null
+                    );
+                }
+                adapter.setDidSend(true);
+                adapter.notifyDataSetChanged();
+                Toast.makeText(getApplicationContext(), contacts.size() + "명에게 전송완료!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         contacts = (ArrayList<Contact>) getIntent().getSerializableExtra("contacts");
-        ListViewAdapter adapter = new ListViewAdapter(contacts);
+        didSend = new ArrayList<>();
+        for (int i = 0; i < contacts.size(); i++) {
+            didSend.add(false);
+        }
+        adapter = new ListViewAdapter(contacts, didSend);
         lv_recipients.setAdapter(adapter);
     }
 }
@@ -43,9 +69,16 @@ public class SendToGroupActivity extends AppCompatActivity {
 class ListViewAdapter extends BaseAdapter {
     LayoutInflater inflater = null;
     private ArrayList<Contact> data = null;
+    private boolean didSend = false;
+    private ArrayList<Boolean> didReply = null;
 
-    public ListViewAdapter(ArrayList<Contact> data) {
+    public ListViewAdapter(ArrayList<Contact> data, ArrayList<Boolean> didReply) {
         this.data = data;
+        this.didReply = didReply;
+    }
+
+    public void setDidSend(boolean didSend) {
+        this.didSend = didSend;
     }
 
     @Override
@@ -75,8 +108,15 @@ class ListViewAdapter extends BaseAdapter {
 
         TextView tv_name_item = view.findViewById(R.id.tv_name_item);
         TextView tv_phone_item = view.findViewById(R.id.tv_phone_item);
+        TextView tv_dot = view.findViewById(R.id.tv_dot);
         tv_name_item.setText(data.get(i).getName());
         tv_phone_item.setText(data.get(i).getPhonenum());
+        if (!didSend) {
+            tv_dot.setVisibility(View.INVISIBLE);
+        } else {
+            tv_dot.setVisibility(View.VISIBLE);
+            tv_dot.setBackgroundColor(didReply.get(i) ? Color.GREEN : Color.RED);
+        }
 
         return view;
     }
